f91c70f6bf74b17ac3678c8f8ca7aa5d66c98e1e
yashnotfound404 <yashmishra.del404@gmail.com>
2024-05-20 23:46:31 +0530

    Final Indents

--- Movie Management.py
+++ Movie Management.py
@@ -97,138 +97,126 @@ else:
 
 
 print('''
+      
 ██╗░░░██╗
 ╚██╗░██╔╝
 ░╚████╔╝░
 ░░╚██╔╝░░
 ░░░██║░░░ash Mishra
-░░░╚═╝░░░'''
- )
-print('\n'*100)
-print('''
-┏┓┏┓┏┳━━━┳┓╋╋┏━━━┳━━━┳━┓┏━┳━━━┓
-┃┃┃┃┃┃┏━━┫┃╋╋┃┏━┓┃┏━┓┃┃┗┛┃┃┏━━┛
-┃┃┃┃┃┃┗━━┫┃╋╋┃┃╋┗┫┃╋┃┃┏┓┏┓┃┗━━┓
-┃┗┛┗┛┃┏━━┫┃╋┏┫┃╋┏┫┃╋┃┃┃┃┃┃┃┏━━┛
-┗┓┏┓┏┫┗━━┫┗━┛┃┗━┛┃┗━┛┃┃┃┃┃┃┗━━┓
-╋┗┛┗┛┗━━━┻━━━┻━━━┻━━━┻┛┗┛┗┻━━━┛
-╋┏┓
-┏┛┗┓
-┗┓┏╋━━┓
-╋┃┃┃┏┓┃
-╋┃┗┫┗┛┃
-╋┗━┻━━┛
-┏━━━┳━━━┳┓╋╋┏┓
-┃┏━┓┃┏━┓┃┗┓┏┛┃
-┃┃╋┃┃┗━┛┣┓┗┛┏┛
-┃┗━┛┃┏┓┏┛┗┓┏┛
-┃┏━┓┃┃┃┗┓╋┃┃
-┗┛╋┗┻┛┗━┛╋┗┛''')
+░░░╚═╝░░░
+
+''')
+print('\n'*20)
+
+
+#Functioning
+
+
 def sup():
- naam = str(input("Enter the movie name :"))
- phno = int(input("Enter phone number :"))
- tic = input("Enter total tickets :")
- fnam = str(input("Enter your first name :"))
- lnam = str(input("Enter your last name :"))
- pswd = input("Enter your passwd :")
- idd = [0]
- usrid = 0
- cur.execute('select usrid from movie')
- for x in cur:
- idd += x
- iddd = 0
- while iddd in idd:
- iddd = random.randint(1000, 3000)
- usrid = iddd
- else:
- print("User ID is: ", usrid)
- v_ins = "insert into movie values( '{}','{}','{}','{}','{}','{}','{}')".format(
- naam, phno, tic, fnam, lnam, pswd, usrid)
- cur.execute(v_ins)
- db.commit()
- print(" General Ticket Booked \n\n")
- mnu()
+    naam = str(input("Enter the movie name :"))
+    phno = int(input("Enter phone number :"))
+    fnam = str(input("Enter your first name :"))
+    lnam = str(input("Enter your last name :"))
+    tic = input("Enter total tickets :")
+    pswd = input("Enter your passwd :")
+    idd = [0]
+    usrid = 0
+    cur.execute('select usrid from movie')
+    for x in cur:
+        idd += x
+    iddd = 0
+    while iddd in idd:
+        iddd = random.randint(1000, 3000)
+        usrid = iddd
+    else:
+        print("User ID is: ", usrid)
+    v_ins = "insert into movie values( '{}','{}','{}','{}','{}','{}','{}')".format(naam, phno, tic, fnam, lnam, pswd, usrid)
+    cur.execute(v_ins)
+    db.commit()
+    print(" General Ticket Booked \n\n")
+    mnu()
+
+
 def frt():
- print("Front Row Booking")
- aa = input("Enter your movie name : ")
- bb = input("Enter your first name : ")
- ee = input("Enter your last name: ")
- cc = int(input("Enter total tickets : "))
- dd = int(input("Enter your ph_no : "))
- pswd = input("Enter the password : ")
- idd = [0]
- usrid = 0
- cur.execute('select usrid from frow')
- for x in cur:
- idd += x
- iddd = 0
- while iddd in idd:
- iddd = random.randint(1, 1000)
- usrid = iddd
- else:
- print("User ID is: ", usrid)
- v_ins = "insert into frow values( '{}','{}','{}','{}','{}','{}','{}')".format(
- aa, dd, cc, bb, ee, pswd, usrid)
- cur.execute(v_ins)
- db.commit()
- print(" **TICKET BOOKED** \n\n")
- mnu()
+    print("Front Row Booking")
+    aa = input("Enter your movie name : ")
+    bb = input("Enter your first name : ")
+    ee = input("Enter your last name: ")
+    cc = int(input("Enter total tickets : "))
+    dd = int(input("Enter your ph_no : "))
+    pswd = input("Enter the password : ")
+    idd = [0]
+    usrid = 0
+    cur.execute('select usrid from frow')
+    for x in cur:
+        idd += x
+        iddd = 0
+    while iddd in idd:
+        iddd = random.randint(1, 1000)
+        usrid = iddd
+    else:
+        print("User ID is: ", usrid)
+    v_ins = "insert into frow values( '{}','{}','{}','{}','{}','{}','{}')".format(
+    aa, dd, cc, bb, ee, pswd, usrid)
+    cur.execute(v_ins)
+    db.commit()
+    print(" **TICKET BOOKED** \n\n")
+    mnu()
 def upcb():
- print("Upper Circle booking")
- a = input("Enter your name : ")
- b = input("Enter your movie name : ")
- c = int(input("Enter your ph_no : "))
- d = int(input("Enter total tickets : "))
- pswd = input("Enter the password : ")
- idd = [0]
- usrid = 0
- cur.execute('select usrid from upcb')
- for x in cur:
- idd += x
- iddd = 0
- while iddd in idd:
- iddd = random.randint(3000, 9999)
- usrid = iddd
- else:
- print("User ID is: ", usrid)
- v_ins = "insert into upcb values( '{}','{}','{}','{}','{}','{}')".format(
- b, c, d, a, pswd, usrid)
- cur.execute(v_ins)
- db.commit()
- print(" ENJOY THE MOVIE AND HAVE FUN \n\n")
- mnu()
+    print("Upper Circle booking")
+    a = input("Enter your name : ")
+    b = input("Enter your movie name : ")
+    c = int(input("Enter your ph_no : "))
+    d = int(input("Enter total tickets : "))
+    pswd = input("Enter the password : ")
+    idd = [0]
+    usrid = 0
+    cur.execute('select usrid from upcb')
+    for x in cur:
+        idd += x
+    iddd = 0
+    while iddd in idd:
+        iddd = random.randint(3000, 9999)
+        usrid = iddd
+    else:
+        print("User ID is: ", usrid)
+    v_ins = "insert into upcb values( '{}','{}','{}','{}','{}','{}')".format(
+    b, c, d, a, pswd, usrid)
+    cur.execute(v_ins)
+    db.commit()
+    print(" ENJOY THE MOVIE AND HAVE FUN \n\n")
+    mnu()
 def delt():
- d = int(input('''What ticket is to be deleted:
- 1. General ticket
- 2. Front row ticket
- 3. Upper circle ticket
- 0. Abort
- [1/2/3] : '''))
- t = ''
- if d == 1:
- e = int(input("Enter the User ID: "))
- l = input("Enter Password : ")
- t = 'movie'
- elif d == 2:
- e = int(input("Enter the User ID: "))
- l = input("Enter Password : ")
- t = 'frow'
- elif d == 3:
- e = int(input("Enter the User ID: "))
- l = input("Enter Password : ")
- t = 'upcb'
- elif d == 0:
- mnu()
- else:
- print('Invalid Input\n')
+    d = int(input('''What ticket is to be deleted:
+    1. General ticket
+    2. Front row ticket
+    3. Upper circle ticket
+    0. Abort
+    [1/2/3] : '''))
+    t = ''
+    if d == 1:
+        e = int(input("Enter the User ID: "))
+        l = input("Enter Password : ")
+        t = 'movie'
+    elif d == 2:
+        e = int(input("Enter the User ID: "))
+        l = input("Enter Password : ")
+        t = 'frow'
+    elif d == 3:
+        e = int(input("Enter the User ID: "))
+        l = input("Enter Password : ")
+        t = 'upcb'
+    elif d == 0:
+        mnu()
+    else:
+        print('Invalid Input\n')
  
 mnu()
- cur.execute(
- "delete from {t} where usrid=\'{e}\' and pswd=\'{l}\'".format(t=t, e=e, l=l))
- db.commit()
- mnu()
+    cur.execute("delete from {t} where usrid=\'{e}\' and pswd=\'{l}\'".format(t=t, e=e, l=l))
+    db.commit()
+    mnu()
 def com():
- co = input('''
+    co = input('''
  ______________________________
  WARNING 
  You may end up loosing data if
@@ -237,56 +225,58 @@ def com():
  do not terminate your commands
  ______________________________
 : ''')
- cur.execute(co)
- om = cur.fetchall()
- for x in om:
- print(x)
- print("\n\n\n")
- mnu()
+    cur.execute(co)
+    om = cur.fetchall()
+    for x in om:
+    print(x)
+    print("\n\n\n")
+    mnu()
+
+
 def mnu():
- print('\n♫♪.ılılıll|̲̅̅ ̲̅̅●̲̲̅̅̅̅ ̲̅̅|=̲̅̅ ̲̅̅|●̲̲̅̅̅̅ ̲̅̅|llılılı.♫♪')
- print("\n1. General Ticket")
- print("2. Front row ticket booking")
- print("3. Upper circle booking")
- print("4. see table")
- print("5. Cancel ticket")
- print("6. Enter command")
- print("7. Exit\n")
- ch = input("Enter your choice [1/2/3/4/5/6/7] :")
- if ch == '1':
- sup()
- elif ch == '2':
- frt()
- elif ch == '3':
- upcb()
- elif ch == '5':
- delt()
- elif ch == '6':
- com()
- elif ch == '7':
- print('''
+    print('\n♫♪.ılılıll|̲̅̅ ̲̅̅●̲̲̅̅̅̅ ̲̅̅|=̲̅̅ ̲̅̅|●̲̲̅̅̅̅ ̲̅̅|llılılı.♫♪')
+    print("\n1. General Ticket")
+    print("2. Front row ticket booking")
+    print("3. Upper circle booking")
+    print("4. see table")
+    print("5. Cancel ticket")
+    print("6. Enter command")
+    print("7. Exit\n")
+    ch = input("Enter your choice [1/2/3/4/5/6/7] :")
+    if ch == '1':
+        sup()
+    elif ch == '2':
+        frt()
+    elif ch == '3':
+        upcb()
+    elif ch == '5':
+        delt()
+    elif ch == '6':
+        com()
+    elif ch == '7':
+        print('''
 ▀█▀ █░█ ▄▀█ █▄░█ █▄▀ █▄█ █▀█ █░█
 ░█░ █▀█ █▀█ █░▀█ █░█ ░█░ █▄█ █▄█
 █▀▀ █▀█ █▀█ █░█ █ █▀ █ ▀█▀ █ █▄░█ █▀▀ ░
 █▀░ █▄█ █▀▄ ▀▄▀ █ ▄█ █ ░█░ █ █░▀█ █▄█ ▄''')
- quit()
- elif ch == '4':
- tbs = []
- cur.execute("Show tables")
- for x in cur:
- tbs += x
- print("What table you want to view ", tbs)
- a = input("Enter Table name : ")
- if a in tbs:
- cur.execute("select * from {}".format(a))
- select = cur.fetchall()
- for x in select:
- print('\n', x)
- print("\n")
- else:
- print('Wrong Input\n')
- mnu()
- else:
- print('Invalid Input.\nQuitting')
- quit()
+    quit()
+    elif ch == '4':
+        tbs = []
+        cur.execute("Show tables")
+        for x in cur:
+            tbs += x
+        print("What table you want to view ", tbs)
+        a = input("Enter Table name : ")
+        if a in tbs:
+            cur.execute("select * from {}".format(a))
+            select = cur.fetchall()
+            for x in select:
+                print('\n', x)
+            print("\n")
+        else:
+            print('Wrong Input\n')
+        mnu()
+    else:
+        print('Invalid Input.\nQuitting')
+        quit()
 mnu()
